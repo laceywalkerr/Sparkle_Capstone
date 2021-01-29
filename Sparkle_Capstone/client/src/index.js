ee2869e660724b005eb05d0facba92af08fe3d54
Lacey Walker <bluelarksong@gmail.com>
2021-01-29 12:23:13 -0600

    updated index.js file and added the call to firebase

--- Sparkle_Capstone/client/src/index.js
+++ Sparkle_Capstone/client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
