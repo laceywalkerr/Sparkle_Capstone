f1e883036dcea2413c0bd61e54f95073c211268c
Lacey Walker <bluelarksong@gmail.com>
2021-01-29 15:31:25 -0600

    tweaked the navbar a little, got rid of an error

--- Sparkle_Capstone/client/src/components/AppHeader.js
+++ Sparkle_Capstone/client/src/components/AppHeader.js
@@ -3,9 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 // import { toast } from "react-toastify";
 import {
     Collapse,
-    Navbar,
     NavbarToggler,
-    NavbarBrand,
     Nav,
     NavItem,
     NavLink,
@@ -32,8 +30,8 @@ const AppHeader = () => {
 
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand">
-                    <img src="https://i.imgur.com/14LECtI.png" alt="Sparkle Logo" width="110" height="70" />
+                <a class="navbar-brand" href="http://localhost:3000/">
+                    <img src="https://i.imgur.com/iVblKqz.png" alt="Sparkle Logo" width="130" height="50" />
                 </a> <br></br>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -96,75 +94,6 @@ const AppHeader = () => {
 
             </nav>
         </div>
-
-
-        // <div>
-        //     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        //         <a class="navbar-brand" href="#">
-        //             <img src="https://i.imgur.com/14LECtI.png" width="110" height="70" />
-        //         </a> <br></br>
-        //         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span class="navbar-toggler-icon"></span>
-        //         </button>
-
-        //         <div class="collapse navbar-collapse" id="navbarColor01">
-        //             <ul class="navbar-nav mr-auto">
-        //                 <li class="nav-item active">
-        //                     <a class="nav-link" href="#">My Reviews
-        //             <span class="sr-only">(current)</span>
-        //                     </a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a class="nav-link" href="#">Explore</a>
-        //                 </li>
-        //                 <li class="nav-item">
-        //                     <a class="nav-link" href="#">Categories</a>
-        //                 </li>
-
-        //             </ul>
-        //             <form class="form-inline my-2 my-lg-0">
-        //                 <input class="form-control mr-sm-2" type="text" placeholder="Search" />
-        //                 <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        //             </form>
-
-        //             <>
-        //                 <NavItem>
-        //                     <NavLink onClick={logoutAndReturn} tag={Link}>
-        //                         Logout
-        //                             </NavLink>
-        //                 </NavItem>
-        //             </>
-        //                 ) : (
-        //                         <>
-        //                 <NavItem>
-        //                     <NavLink to="/login" tag={Link}>
-        //                         Login
-        //                      </NavLink>
-        //                 </NavItem>
-        //                 <NavItem>
-        //                     <NavLink to="/register" tag={Link}>
-        //                         Register
-        //                                 </NavLink>
-        //                 </NavItem>
-        //             </>
-        //                     )
-
-        //         {user ? (
-        //                 <NavbarText className="d-sm-none d-md-block">
-        //                     Welcome {user.displayName}
-        //                 </NavbarText>
-        //             ) : null}
-
-        //         </div>
-        //     </nav>
-        // </div >
-
-
-
-
-
-
-
 
     );
 };
