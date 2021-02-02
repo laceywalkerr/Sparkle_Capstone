8ee54ec790ebbec858e02c870cd1fd7c579915a3
Lacey Walker <bluelarksong@gmail.com>
2021-02-02 11:52:39 -0600

    added a button for add review

--- Sparkle_Capstone/client/src/components/ReviewList.js
+++ Sparkle_Capstone/client/src/components/ReviewList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReviewSummaryCard from "./ReviewSummaryCard";
+import { Link } from "react-router-dom";
 
 const ReviewList = ({ reviews }) => {
     return (
+
         <div>
+            <p>
+                <Link className="btn btn-primary" to="/reviews/add">Add A Review</Link>
+            </p>
             {reviews.map((review) => (
                 <div className="m-4" key={review.id}>
                     <ReviewSummaryCard review={review} />
